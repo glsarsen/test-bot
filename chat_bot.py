a05ef13f77b881500052a0d2fc44a8c59e6f4bf8
Arsenii Hlazov <glazov.arseniy@gmail.com>
2022-06-21 14:22:47 +0300

    webhook

--- chat_bot.py
+++ chat_bot.py
@@ -54,4 +54,6 @@ def incoming():
 
 if __name__ == "__main__":
     context = ("server.crt", "server.key")
-    app.run(host="http://glsarsen.pythonanywhere.com/", port=443, debug=True, ssl_context=context)
+    app.run(host="0.0.0.0", port=443, debug=True, ssl_context=context)
+
+    viber.set_webhook('https://mybotwebserver.com:443/')
